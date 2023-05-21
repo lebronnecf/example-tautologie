d387ec9bbb701df53f2ad15a6136594a509efb5c
fabienlebronnec <lebronnec.fabien@gmail.com>
2023-05-21 16:30:20 +0200

    Init

---
+++ src/main/java/fr/lebronnecf/EvenHelper.java
@@ -0,0 +1,9 @@
+package fr.lebronnecf;
+
+public class EvenHelper {
+
+    public boolean isEven(int i) {
+        return (i % 2 == 0);
+    }
+
+}
---
+++ src/test/java/fr/lebronnecf/EvenHelperTest.java
@@ -0,0 +1,19 @@
+package fr.lebronnecf;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EvenHelperTest {
+
+    @Test
+    void checkIsEven() {
+        int[] numbers = {1, 2, 3, 4, 5, 23, 70, 89, 240};
+        EvenHelper evenHelper = new EvenHelper();
+
+        for (int n : numbers) {
+            boolean expectedResult = n % 2 == 0;
+            assertEquals(expectedResult, evenHelper.isEven(n));
+        }
+    }
+}
